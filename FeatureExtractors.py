daf674ee4c7e7348994e81b05abcc0664f4a3693
Ashok Koduru <ashok.koduru9@gmail.com>
2017-12-02 04:17:19 -0500

    Adding req code from Pacman

---
+++ FeatureExtractors.py
@@ -0,0 +1,18 @@
+# ---------------------------------------- #
+# Author   : Ashok Koduru, Bhushan Mohite
+# Project  : Blackjack-AI
+# Date     : 02nd Dec, 2017
+# Created in PyCharm
+# ---------------------------------------- #
+
+# Code credits:
+# http://www-inst.eecs.berkeley.edu/~cs188/sp11/projects/reinforcement/docs/featureExtractors.html
+
+import sys, os, util
+
+
+
+
+
+
+
