28e0d6bfa2d63eb412d18f474ba33f5b6603714a
Ashok Koduru <ashok.koduru9@gmail.com>
2017-12-04 01:23:24 -0500

    Dealer bugs corredted and Done

--- Dealer.py
+++ Dealer.py
@@ -5,13 +5,14 @@
 # Created in PyCharm
 # ---------------------------------------- #
 
-import CardandDeck
+import CardDeckandShoe
+
 
 class Dealer:
 
-    def __init__(self, num_of_decks=1, on_hold=False):
+    def __init__(self, num_of_decks=1):
         self.num_of_decks = num_of_decks  # Number of decks in play
-        self.shoe = CardandDeck.Shoe(num_of_decks)  # Creating a new shoe
+        self.shoe = CardDeckandShoe.Shoe(num_of_decks)  # Creating a new shoe
         self.hand_player = []  # Cards in the hand of the player
         self.hand_dealer = []  # Cards in the hand of the dealer
         self.value_player = 0  # Value of the player cards
@@ -19,7 +20,6 @@ class Dealer:
         self.player_contains_ace = False
         self.dealer_contains_ace = False
         self.dealer_won = False
-        self.on_hold = on_hold
         self.cards_discarded = []
         self.game_done = False
         self.shoe.shuffle_shoe()
@@ -58,13 +58,7 @@ class Dealer:
         while True:
             self.play()
             self.value_of_hand()
-            if self.on_hold:
-                return
-            else:
-                self.player_turn()
-            choice = input("\nWant to play another game?\n 1: Y\n 2: N")
-            if choice == 2:
-                break
+            return
 
     def play(self):
         for i in range(2):
@@ -79,7 +73,7 @@ class Dealer:
         return card
 
     def reshuffle_shoe(self):
-        self.shoe = CardandDeck.Shoe(self.num_of_decks)
+        self.shoe = CardDeckandShoe.Shoe(self.num_of_decks)
         self.shoe.shuffle_shoe()
         self.cards_discarded = []
 
@@ -108,16 +102,10 @@ class Dealer:
         self.value_of_hand()
 
         if self.value_dealer > 21:
-            if not self.on_hold:
-                return self.game_over()
-            else:
-                return self.game_over_on_hold()
+            return self.game_over_on_hold()
 
         if self.value_dealer > 16:
-            if not self.on_hold:
-                return self.game_over()
-            else:
-                return self.game_over_on_hold()
+            return self.game_over_on_hold()
 
         while self.value_dealer < 17:
             self.hand_dealer.append(self.get_card_from_shoe())
@@ -125,121 +113,51 @@ class Dealer:
             if self.dealer_contains_ace:
                 if ace_present:
                     if self.value_dealer == 21:
-                        if self.on_hold:
-                            return self.game_over_on_hold()
-                        else:
-                            self.dealer_contains_ace = False
-                            return self.game_over()
+                        return self.game_over_on_hold()
                     if self.value_dealer > 21:
                         ace_present = False
                         self.value_dealer -= 10
-                if self.value_dealer + 10 < 22:
+                if self.value_dealer < 12:
                     ace_present = True
                     self.value_dealer += 10
 
             if self.value_dealer > 21:
-                if self.on_hold:
-                    return self.game_over_on_hold()
-                else:
-                    return self.game_over()
+                return self.game_over_on_hold()
 
             if self.value_dealer > 16:
                 if ace_present:
                     self.value_dealer -= 10
                 else:
-                    if self.on_hold:
-                        return self.game_over_on_hold()
-                    else:
-                        return self.game_over()
+                    return self.game_over_on_hold()
 
     def player_turn(self, action=2):
         self.value_of_hand()
-
         if self.value_player > 21:
-            if self.on_hold:
-                return self.game_over_on_hold()
-            else:
-                return self.game_over()
+            return self.game_over_on_hold()
 
-        if self.value_player is 21:
-            if self.on_hold:
-                return self.game_over_on_hold()
-            else:
-                return self.game_over()
+        if self.value_player == 21:
+            return self.game_over_on_hold()
 
         if self.player_contains_ace:
-            if self.on_hold:
-                if self.value_player + 10 == 21:
-                    return self.game_over_on_hold()
-            else:
-                if self.value_player + 10 == 21:
-                    self.value_player += 10
-                    self.player_contains_ace = False
-                    return self.game_over()
+            if self.value_player == 11:
+                return self.game_over_on_hold()
 
         while self.value_player < 22:
-            if self.on_hold:
-                if action == 2:
-                    return self.dealer_turn()
-                else:
-                    self.hand_player.append(self.get_card_from_shoe())
-                    self.value_of_hand()
-                    if self.value_player > 21:
-                        return self.game_over_on_hold()
-
-                    if self.value_player == 21:
-                        return self.game_over_on_hold()
-
-                    if self.player_contains_ace:
-                        if self.value_player + 10 == 21:
-                            return self.game_over_on_hold()
-                    return False
+            if action is 2:
+                return self.dealer_turn()
             else:
-                choice = input("\nPlease Enter\n 1. Hit\n 2. Stand")
-                if choice == 2:
-                    self.dealer_turn()
-                    return
-
                 self.hand_player.append(self.get_card_from_shoe())
                 self.value_of_hand()
-
                 if self.value_player > 21:
-                    self.game_over()
-                    return
+                    return self.game_over_on_hold()
 
                 if self.value_player == 21:
-                    self.dealer_turn()
-                    return
+                    return self.game_over_on_hold()
 
                 if self.player_contains_ace:
-                    if self.value_player + 10 == 21:
-                        self.value_player += 10
-                        self.player_contains_ace = False
-                        self.dealer_turn()
-                        return
-
-    def game_over(self):
-
-        self.hand_player = []
-        self.hand_dealer = []
-
-        if self.value_player > 21:
-            self.dealer_won = False
-        elif self.value_dealer > 21:
-            self.dealer_won = True
-        elif self.value_player == self.value_dealer:
-            self.dealer_won = False
-            return True
-        elif self.value_player == 21:
-            self.dealer_won = True
-        elif self.value_dealer == 21:
-            self.dealer_won = False
-        elif self.value_player > self.value_dealer:
-            self.dealer_won = True
-        elif self.value_player < self.value_dealer:
-            self.dealer_won = False
-
-        return True
+                    if self.value_player == 11:
+                        return self.game_over_on_hold()
+                return False
 
     def game_over_on_hold(self):
         for i in range(len(self.hand_player)):
@@ -263,5 +181,4 @@ class Dealer:
             self.dealer_won = True
         elif self.value_player < self.value_dealer:
             self.dealer_won = False
-
         return True
